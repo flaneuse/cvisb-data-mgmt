59e0f14b711024a0b4f40cd8f96bae7f08640f31
Laura Hughes <flaneuse@users.noreply.github.com>
2018-03-14 14:34:33 -0700

    initial routes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# CvisbDataMgmt
+# CViSBDataMgmt
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.5.5.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PatientComponent } from './patient/patient.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
+import { AppRoutingModule } from './/app-routing.module';
+import { MaterialModule } from './material.module';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    PatientComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
