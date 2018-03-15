62c2cbf88e97ccbb335c9979a05b5b025cf6e0a9
Laura Hughes <flaneuse@users.noreply.github.com>
2018-03-15 09:14:03 -0700

    finished patient info navbar prototype

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 
-import { MatAutocompleteModule, MatFormFieldModule, MatInputModule} from '@angular/material';
+import {
+  MatAutocompleteModule, MatFormFieldModule, MatInputModule,
+  MatChipsModule
+} from '@angular/material';
 
 @NgModule({
-  imports: [MatAutocompleteModule, MatFormFieldModule, MatInputModule],
-  exports: [MatAutocompleteModule, MatFormFieldModule, MatInputModule],
+  imports: [MatAutocompleteModule, MatFormFieldModule, MatInputModule,
+    MatChipsModule
+  ],
+  exports: [MatAutocompleteModule, MatFormFieldModule, MatInputModule,
+    MatChipsModule],
 })
 export class MaterialModule { }
