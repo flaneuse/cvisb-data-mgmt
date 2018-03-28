a3cd031b829039b8b29a44557399d0cb3232ccb2
Laura Hughes <flaneuse@users.noreply.github.com>
2018-03-28 14:23:36 -0700

    add timepoint filterability

--- src/app/patient-header/patient-header.component.ts
+++ src/app/patient-header/patient-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnChanges, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, OnChanges, Output, EventEmitter, ChangeDetectorRef } from '@angular/core';
 
 import { FormControl } from "@angular/forms";
 import { Observable } from 'rxjs/Observable';
@@ -10,15 +10,10 @@ import { map } from 'rxjs/operators/map';
 import { Patient } from '../classes/patient';
 import { GetPatientRosterService } from "../data-services/get-patient-roster.service"
 
-export class User {
+export class Timepoint {
   constructor(
-    public name: string,
-    public patient_id: string,
-    public sex: string,
-    public age: number,
-    public cohort: string,
-    public cohort_exposure: string,
-    public timepoints: Array<number>
+    public timepoint: number,
+    public state: boolean
   ) { }
 }
 
@@ -30,30 +25,31 @@ export class User {
 })
 export class PatientHeaderComponent implements OnInit {
   @Output() patientEmitter = new EventEmitter<Patient>();
+  @Output() timeptEmitter = new EventEmitter<number>();
 
   private patients: Array<Patient> = [];
-  // private patientIDs: Array<string> = [];
   currentPatient: Object;
+  timepoints: Array<Timepoint> = [];
+  selTimepoint: number;
 
   patientCtrl: FormControl = new FormControl();
-  options: any;
+  // options: any;
   filteredOptions: Observable<Patient[]>;
 
-  constructor(private roster: GetPatientRosterService) {
+  constructor(private roster: GetPatientRosterService, private _changeDetectionRef: ChangeDetectorRef) {
   }
 
 
 
   ngOnInit() {
     this.patients = this.roster.createFakePatients();
-    this.options = this.patients;
-
+    console.log(this.patients)
 
     this.filteredOptions = this.patientCtrl.valueChanges
       .pipe(
-      startWith<string | Patient>(''),
-      map(value => typeof value === 'string' ? value : value.patient_id),
-      map(patient_id => patient_id ? this.filter(patient_id) : this.options.slice())
+        startWith<string | Patient>(''),
+        map(value => typeof value === 'string' ? value : value.patient_id),
+        map(patient_id => patient_id ? this.filter(patient_id) : this.patients.slice())
       );
   }
 
@@ -68,24 +64,61 @@ export class PatientHeaderComponent implements OnInit {
   }
 
 
-  ngOnChanges() {
+  // add booleans for timepoints to track if they've been selected
+  getTimepoints(tp) {
+    let arr = [];
+
+    for (var i = 0; i < tp.length; i++) {
+      arr.push(new Timepoint(tp[i], false));
+
+    }
+    return arr;
   }
 
-  // For Patient ID select, only display the ID
+  // For Patient ID select, only display the ID, not the entire object
   displayFunc(user?): string | undefined {
     return user ? user.patient_id : undefined;
   }
 
   changeID(event, new_patient) {
-    console.log("CHANGED!")
+    // Fix for double-sending events if click/keypress; see https://github.com/angular/material2/issues/4094
     if (event.isUserInput) {
-      // Fix for double-sending events if click/keypress; see https://github.com/angular/material2/issues/4094
-      console.log(new_patient.patient_id)
       // Update current patient
       this.currentPatient = new_patient;
       this.patientEmitter.emit(new_patient);
+      this.timepoints = this.getTimepoints(new_patient.timepoints);
+      // Reset the selected timepoint if already selected.
+      this.selTimepoint = null;
+      this.timeptEmitter.emit(null);
     }
   }
 
+
+  highlightTimepoint(event, timept) {
+    // console.log(timept)
+
+    let vm = this;
+    // workaround to deal w/ asynch changes b/w parent (patient-header) and child (mat-chip)
+    // see https://stackoverflow.com/questions/47021161/incorrect-behaviour-when-selecting-chips-in-angular-material?rq=1
+    setTimeout(function() {
+
+      // Change the state of the selected
+      timept.state = !timept.state;
+
+      vm._changeDetectionRef.detectChanges();
+    }, 5);
+
+    // Situation 1: turn on the timepoint marker
+    if (timept.state === false) {
+      // reset all states to false
+      this.timepoints.find(d => d.timepoint !== timept.timepoint).state = false;
+      this.selTimepoint = timept.timepoint;
+    } else {
+      this.selTimepoint = null;
+    }
+
+    this.timeptEmitter.emit(this.selTimepoint);
+
+  }
 
 }
