e43d9a3682a4930ece6528eb485ec3ab1e2bcdb6
Laura Hughes <flaneuse@users.noreply.github.com>
2018-04-02 10:58:26 -0700

    update file name structure

--- src/app/classes/filename.ts
+++ src/app/classes/filename.ts
@@ -3,22 +3,43 @@ export class Filename {
   public timepoint: number;
   public sample_id: string;
   public expt_id: string;
+  public date_expt?: Date;
   public ext?: string;
+  public opts?: string;
 
   constructor(options: any) {
     this.patient_id = options.patient_id;
     this.timepoint = options.timepoint;
     this.sample_id = options.sample_id;
     this.expt_id = options.expt_id;
+    this.date_expt = options.date_expt;
     this.ext = options.ext;
+    this.opts = options.opts;
   }
 
   concat(): string {
-    if (this.ext) {
-      return this.patient_id + '_T' + this.timepoint + this.sample_id + '_' + this.expt_id + this.ext;
-    } else {
-      return this.patient_id + '_T' + this.timepoint + this.sample_id + '_' + this.expt_id;
+    let beg = this.patient_id + '_T' + this.timepoint + '-' + this.sample_id + '_' + this.expt_id;
+
+    let mid = '';
+
+    let date = '';
+
+    let end = '';
+
+    if(this.opts) {
+      mid = this.opts;
     }
+
+    if(this.date_expt) {
+      // REMEMBER: months are 0-indexed in js
+      date = "_" + this.date_expt.getFullYear() + "-" + String(this.date_expt.getMonth() + 1).padStart(2,"0") + "-" + String(this.date_expt.getDate()).padStart(2,"0");
+    }
+
+    if (this.ext) {
+      end = this.ext;
+    }
+
+    return beg + mid + date + end;
   }
 }
 
